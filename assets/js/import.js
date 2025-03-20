9bb2249bd02a2a082774bde58068028869235105
Matt Ryan <mattryanco@proton.me>
2025-03-20 16:18:29 -0400

    Update JS with add'l progress messages.

--- assets/js/import.js
+++ assets/js/import.js
@@ -3,7 +3,10 @@ jQuery(document).ready(function($) {
     function handleImportClick(event) {
         event.preventDefault();
         var licenseImportFile = $('a.rwmb-file-title').attr('href');
+        var recordLimit = $('#record_limit').val();
+        var recordLimit = recordLimit ? recordLimit : 0;
         console.log('License import file: ' + licenseImportFile);
+        console.log('Record Limit: ' + recordLimit);
 
         if (!licenseImportFile) {
             alert('Please select a license import file from the media library. If you do not have a file to import, please upload one.');
@@ -16,20 +19,30 @@ jQuery(document).ready(function($) {
             url: import_data.ajax_url,
             type: 'POST',
             data: {
-                action: 'capweb_import_licenses',
-                license_import_file: licenseImportFile
+                action: 'capweb_import_licenses_callback',
+                license_import_file: licenseImportFile,
+                record_limit: recordLimit,
             },
-            success: handleAjaxSuccess,
-            error: handleAjaxError
+            success: function(response) {
+                console.log('AJAX Success:', response);
+                handleAjaxSuccess(response);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error('AJAX Error:', textStatus, errorThrown, jqXHR.responseText);
+                handleAjaxError(jqXHR, textStatus, errorThrown, licenseImportFile);
+            }
         });
+
     }
 
     function handleAjaxSuccess(response) {
-        $('#import-results').html(response);
+        // $('#import-results').html(response);
+        $('#import-results').html('<h2>Success!</h2><br>' + response );
+        console.log('Success!');
     }
 
-    function handleAjaxError( jqXHR, textStatus, errorThrown) {
-        $('#import-results').html('<p>A(n) ' + textStatus + '--- ' + errorThrown + ' ---- occurred while processing your request.<br>Please try again.</p>');
+    function handleAjaxError(jqXHR, textStatus, errorThrown, licenseImportFile) {
+        $('#import-results').html('<p>A(n) ' + textStatus + '--- ' + errorThrown + ' ---- occurred while processing ' + licenseImportFile + '.<br>Please try again.</p>');
     }
 
     // Ensure the document is ready before attaching the event handler
